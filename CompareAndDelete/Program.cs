856887f942bb75371ff3b7c7042ab6f81a3fcb47
JD Marymee <jmarymee@microsoft.com>
2017-08-08 12:31:29 -0700

    Combined core and dotnet code

---
+++ CompareAndDelete/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CompareDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string referencePathCore = @"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\1.1.2";
+            string referencePathMLModule = @"C:\Users\jmarymee\Documents\Visual Studio 2017\Projects\iotedgeml\EdgeModule\bin\Debug";
+            string sourcePath = @"C:\repos\IOT_Bakeoff\Simulator\EdgeContainer\iot-edge";
+            string fileListFile = @"c:\tools\filesCopied.csv";
+
+            CompareAndDeleteCore(sourcePath, referencePathCore);
+            //CompareAndCopyMLModuleCode(sourcePath, referencePathMLModule, fileListFile);
+            CopyMLFilesToTarget(sourcePath, referencePathMLModule, fileListFile);
+            //RemoveMLModuleUniqueFiles(sourcePath, fileListFile);
+
+            Console.WriteLine("All done. Any key...");
+            Console.ReadLine();
+        }
+
+        public static void RemoveMLModuleUniqueFiles(string src, string fileListFile)
+        {
+            if (!File.Exists(fileListFile)) { return; }
+            string[] fileListToRemove = File.ReadAllLines(fileListFile);
+            foreach (string s in fileListToRemove)
+            {
+                string fName = Path.GetFileName(s);
+                string targetToDelete = src + Path.DirectorySeparatorChar + fName;
+                File.Delete(targetToDelete);
+            }
+
+            Console.WriteLine(String.Format("{0} Files deleted", fileListToRemove.Length));
+        }
+
+        public static void CopyMLFilesToTarget(string src, string refPath, string fileListFile)
+        {
+            if (!File.Exists(fileListFile)) { return; }
+            string[] fileList = File.ReadAllLines(fileListFile);
+
+            foreach (string s in fileList)
+            {
+                string fileName = Path.GetFileName(s);
+                string target = src + Path.DirectorySeparatorChar + fileName;
+
+                File.Copy(s, target);
+            }
+
+            Console.WriteLine(String.Format("{0} Files copied", fileList.Length));
+        }
+
+        public static void CompareAndCopyMLModuleCode(string src, string refPath, string fileListFile)
+        {
+            IEnumerable<string> refFiles = Directory.EnumerateFiles(refPath);
+            List<string> fileList = new List<string>();
+
+            foreach (string s in refFiles)
+            {
+                string target = src + Path.DirectorySeparatorChar + Path.GetFileName(s);
+                if (!File.Exists(target)) { fileList.Add(s); }
+            }
+
+            File.WriteAllLines(fileListFile, fileList.ToArray());
+        }
+
+        public static void CompareAndDeleteCore(string src, string refPath)
+        {
+            IEnumerable<string> refFiles = Directory.EnumerateFiles(refPath);
+            foreach (string file in refFiles)
+            {
+                string fName = Path.GetFileName(file);
+                string cPath = src + Path.DirectorySeparatorChar + fName;
+                if (File.Exists(cPath))
+                {
+                    Console.WriteLine(String.Format("{0} is a duplicate", cPath));
+                    File.Delete(cPath);
+                }
+            }
+        }
+    }
+}
--- DotNetInspector/Program.cs
+++ DotNetInspector/Program.cs
@@ -1,15 +1,88 @@
 ﻿using System;
+using System.Reflection;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNetInspector
+namespace DotNetInspectorGadget
 {
-    class Program
+    class DotNetInspectorGadget
     {
         static void Main(string[] args)
         {
+            string srcPath = @"C:\repos\IOT_Bakeoff\Simulator\EdgeContainer\iot-edge";
+            string assemplyName = @"System.ComponentModel.TypeConverter";
+
+            string assembly = @"C:\Users\jmarymee\stage\stage2\System.ComponentModel.TypeConverter.dll";
+
+            InspectAnAssembly(assembly);
+
+            FindThatDependency(srcPath, assemplyName);
+
+            ////Console.WriteLine(Directory.GetCurrentDirectory());
+            //if (args.GetLength(0) < 1)
+            //{
+            //    Console.WriteLine("Add a single parameter that is your" +
+            //    " path to the file you want inspected.");
+            //    return 1;
+            //}
+            //try
+            //{
+            //    string fullPath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + args[0];
+            //    Console.WriteLine(fullPath);
+            //    //var assemblies = Assembly.LoadFile(@args[0]).GetReferencedAssemblies();
+            //    var assemblies = Assembly.LoadFile(fullPath).GetReferencedAssemblies();
+
+            //    if (assemblies.GetLength(0) > 0)
+            //    {
+            //        foreach (var assembly in assemblies)
+            //        {
+            //            Console.WriteLine(assembly);
+            //        }
+            //        return 0;
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("An exception occurred: {0}", e.Message);
+            //    return 1;
+            //}
+            //finally { }
+
+            //return 1;
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        public static void InspectAnAssembly(string path)
+        {
+            Assembly a = Assembly.LoadFile(path);
+            AssemblyName[] aNames =a.GetReferencedAssemblies();
+        }
+
+        public static void FindThatDependency(string path, string assemblyName)
+        {
+            IEnumerable<string> dlls = Directory.EnumerateFiles(path, "*.dll");
+            foreach(string s in dlls)
+            {
+                try
+                {
+                    AssemblyName[] aNames = Assembly.LoadFile(s).GetReferencedAssemblies();
+                    foreach (AssemblyName name in aNames)
+                    {
+                        if (name.Name.Equals(assemblyName))
+                        {
+                            Console.WriteLine(String.Format("The requiring assembly is: {0}", Path.GetFileName(s)));
+                            Console.WriteLine(name);
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                catch (Exception exp)
+                {
+                    //Console.WriteLine(exp.Message);
+                }
+            }
+
         }
     }
 }
